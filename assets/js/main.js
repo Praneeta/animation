45da6bd015aee725b2c38585444e23632f77b68a
akotian <ashley.kotian@livingly.com>
2014-12-19 15:53:28 -0800

    More questions

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,9 @@ var Animation = Animation || {};
 
 Animation.mainFunction = (function() {
   var new_gift_class = "new-gift",
-      video_links = ['w8vxuZ3bjrA', 'L0MK7qz13bU', 'YJ50YCpYkqM', 'D2va8ddcY4E'],
       special_gift = ['', 'star', 'box', 'bear'],
       gift_num = 0,
       questions_container = ".questions-container",
-      //hidden_question = "hidden-question",
       hidden_question = "hidden-question",
       question_set = [
         {"question":"In home alone what is the name of the kid",
@@ -17,27 +15,42 @@ Animation.mainFunction = (function() {
         {"question":"Where on her head does Anna develop a white streak",
          "answers":["The Right Side", "The Left Side", "The Ceter", "It all turns white"],
          "correct": 3,
-         "video": "L0MK7qz13bU"
+         "video": "j08nbuPc92w"
         },
         {"question":"In Polar Express movie, what is 'The First Gift of Christmas'",
          "answers":["A Reindeer","A bell from Santa's sleigh", "The Polar Star", "Santa Himself"],
          "correct": 2,
-         "video": "YJ50YCpYkqM"
+         "video": "dofECCtTfaM"
         },
         {"question":"What is the name of the rabbit in the maggic hat in Frosty the Snowman",
          "answers":["Hocus Pocus","Abra ka Dabra", "Magic Mike", "Avada Kevadra"],
          "correct": 1,
-         "video": "YJ50YCpYkqM"
+         "video": "8yflEEC-WdE"
         },
-        {"question":"In home alone what is the name of the kid",
-         "answers":["Jack","Mark", "Kevin", "Harry"],
+        {"question":"What did Harry receive from Dumbledore during his first Christmas at Hotwarts",
+         "answers":["The Philosophers Stone","The cloak of invisibility", "A new want", "Socks"],
          "correct": 2,
-         "video": "YJ50YCpYkqM"
+         "video": "w8vxuZ3bjrA"
         },
-        {"question":"In home alone what is the name of the kid",
-         "answers":["Jack","Mark", "Kevin", "Harry"],
+        {"question":"What was the most likely reason that The Grinch hated christmas",
+         "answers":["His heart was too small","He never saw Santa", "He never got presents", "He did not have a heart at all"],
          "correct": 2,
-         "video": "YJ50YCpYkqM"
+         "video": "0lq1JIWQSlc"
+        },
+        {"question":"What present did Harry get from Kreacher in The Half-Blood Price",
+         "answers":["His heart was too small","He never saw Santa", "He never got presents", "He did not have a heart at all"],
+         "correct": 2,
+         "video": "w8vxuZ3bjrA"
+        },
+        {"question":"What was the most likely reason that The Grinch hated christmas",
+         "answers":["His heart was too small","He never saw Santa", "He never got presents", "He did not have a heart at all"],
+         "correct": 2,
+         "video": "w8vxuZ3bjrA"
+        },
+        {"question":"What was the most likely reason that The Grinch hated christmas",
+         "answers":["His heart was too small","He never saw Santa", "He never got presents", "He did not have a heart at all"],
+         "correct": 2,
+         "video": "w8vxuZ3bjrA"
         },
       ],
 
