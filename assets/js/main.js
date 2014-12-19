e7d87a95be8694f314b216b1a5cde0d6fc910376
akotian <ashley.kotian@livingly.com>
2014-12-19 13:04:05 -0800

    New gifts created on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,32 @@
 var Animation = Animation || {};
 
 Animation.mainFunction = (function() {
-  var test = "hello",
-  myTween = function() {
-    //var myTest = $(".test-tween");
-    //TweenLite.to(".test-tween", 5, {width:500});
-    TweenLite.from(".gift", 4, {top: 0, ease:Bounce.easeOut});
-  };
+  var new_gift_class = "new-gift",
+
+      //giftTween = TweenLite.to(".gift", 4, {top: 630, ease:Bounce.easeOut}),
+
+      //myTween = function() {
+      //  //TweenLite.from(".gift", 4, {top: 0, ease:Bounce.easeOut});
+      //  giftTween.pause();
+      //};
+      removeNew = function(target) {
+        target.removeClass(new_gift_class);
+      },
+      createAndDropGift = function() {
+        var new_gift = $('<div class="gift"></div>');
+        new_gift.addClass(new_gift_class);
+        $('.page-body').append(new_gift);
+        TweenLite.to("."+new_gift_class, 4, {top: 630, 
+          ease:Bounce.easeOut, onComplete:function(event){
+            removeNew(new_gift);
+        }});
+      };
   return {
     init: function() {
-      myTween();
+      //myTween();
+      $(".theclick").on("click", function() {
+        createAndDropGift();
+      });
     }
   }
 })();
