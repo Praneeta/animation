6276ac8c83fb5800242f4c8603ade2821620c4d8
akotian <ashleykotian@gmail.com>
2014-12-20 16:47:33 -0800

    Canvas sharing to FB mother fucking works

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,51 @@
-var Animation = Animation || {};
+var Animation = Animation || {},
+    webSite = "http://localhost:8000",
+    fileName = "christmasCard";
+
+
+//https://github.com/lukasz-madon/heroesgenerator/blob/master/script.js
+if ( XMLHttpRequest.prototype.sendAsBinary === undefined ) {
+      XMLHttpRequest.prototype.sendAsBinary = function(string) {
+       var bytes = Array.prototype.map.call(string, function(c) {
+         return c.charCodeAt(0) & 0xff;
+       });
+       this.send(new Uint8Array(bytes).buffer);
+     };
+};
+
+//(function(d, s, id) {
+//    var js, fjs = d.getElementsByTagName(s)[0];
+//      if (d.getElementById(id)) return;
+//        js = d.createElement(s); js.id = id;
+//          js.src = "//connect.facebook.net/en_US/all.js";
+//            fjs.parentNode.insertBefore(js, fjs);
+//}(document, 'script', 'facebook-jssdk'));
+//
+//window.fbAsyncInit = function() {
+//      FB.init({
+//              appId  : "897326893619716",
+//              status : true, 
+//                    cookie : true, 
+//                      xfbml  : true  // parse XFBML
+//                            });
+//};
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '897326893619716',
+    xfbml      : true,
+    version    : 'v2.2'
+  });
+};
+
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 
 Animation.mainFunction = (function() {
   var new_gift_class = "new-gift",
@@ -39,7 +86,7 @@ Animation.mainFunction = (function() {
           $(this).removeClass(hidden_question);
           selectAndPlayVideo($(this), true)
         });
-      }
+      },
 
       handleAnswerClick = function(target) {
         if (target.data("correct")) {
@@ -117,7 +164,61 @@ Animation.mainFunction = (function() {
         var $gift = $(event.target)
         if($gift.attr('data-play-video') === 'true') showNextQuestion();
         $gift.attr('data-play-video', 'false')
-      };
+      },
+      postImageToFacebook = function(authToken, filename, mimeType, imageData, message) {
+        // this is the multipart/form-data boundary we'll use
+        var boundary = '----ThisIsTheBoundary1234567890';
+        // let's encode our image file, which is contained in the var
+        var formData = '--' + boundary + '\r\n';
+          formData += 'Content-Disposition: form-data; name="source"; filename="' + filename + '"\r\n';
+        formData += 'Content-Type: ' + mimeType + '\r\n\r\n';
+        for ( var i = 0, imageLength=imageData.length; i < imageLength; ++i ) {
+          formData += String.fromCharCode( imageData[ i ] & 0xff );
+        }
+        formData += '\r\n';
+        formData += '--' + boundary + '\r\n';
+        formData += 'Content-Disposition: form-data; name="message"\r\n\r\n';
+        formData += message + '\r\n'
+        formData += '--' + boundary + '--\r\n';
+        var xhr = new XMLHttpRequest();
+        xhr.open( 'POST', 'https://graph.facebook.com/me/photos?access_token=' + authToken, true );
+        xhr.onload = xhr.onerror = function() {
+          console.log( xhr.responseText );
+        };
+        xhr.setRequestHeader( "Content-Type", "multipart/form-data; boundary=" + boundary );
+        xhr.sendAsBinary( formData );
+      },
+
+      postCanvasToFacebook = function() {
+        html2canvas($(".page-body"), {
+          onrendered: function(canvas) {
+                //theCanvas = canvas;
+                //document.body.appendChild(canvas);
+                //  var image = $('.canvas-outcome img');
+                //  debugger;
+                //  image.attr("src", canvas.toDataURL("image/png")); 
+            //}
+            debugger;
+            var data = canvas.toDataURL("image/png");
+            var encodedPng = data.substring(data.indexOf(',') + 1, data.length);
+            var decodedPng = Base64Binary.decode(encodedPng);
+            FB.getLoginStatus(function(response) {
+              debugger;
+              if (response.status === "connected") {
+                postImageToFacebook(response.authResponse.accessToken, fileName, "image/png", decodedPng, webSite);
+              } else if (response.status === "not_authorized") {
+                FB.login(function(response) {
+                  postImageToFacebook(response.authResponse.accessToken, fileName, "image/png", decodedPng, webSite);
+                 }, {scope: "publish_actions"});
+              } else {
+                FB.login(function(response)  {
+                  postImageToFacebook(response.authResponse.accessToken, fileName, "image/png", decodedPng, webSite);
+                }, {scope: "publish_actions"});
+             }
+            });
+          }
+        })
+     };
   return {
     init: function() {
       buildQuestionContainer();
@@ -126,27 +227,9 @@ Animation.mainFunction = (function() {
         handleAnswerClick($(this));
       });
       santaGivesGift();
-
      $(".save-image").on("click", function() {
-        console.log("we are in element click---");
-        html2canvas($(".tree"), {
-            onrendered: function(canvas) {
-                theCanvas = canvas;
-                document.body.appendChild(canvas);
-                  var image = $('.canvas-outcome img');
-                  debugger;
-                  image.attr("src", canvas.toDataURL("image/png"));
-                  console.log(image);
-                  //$(body).append(image)
-                //canvas.toBlob(function(blob) {
-		//  //saveAs(blob, "christmastree.png");
-                //  //debugger;
-
-	        //});
-            }
-        });
+       postCanvasToFacebook();
      });
-
     }
   }
 })();
