7ac32e83f1eacc4141e96c5645fb3cde329e62c7
akotian <ashleykotian@gmail.com>
2014-12-23 23:14:14 -0800

    Remove christmas lights, fix console error

--- assets/js/snowman.js
+++ assets/js/snowman.js
@@ -3,3 +3,5 @@ Animation.trivia = false;
 Animation.giftDrop = 560;
 Animation.video = { height: "128.5", width: "211.2" };
 Animation.special_gift = ['bear', 'star-new', 'box'];
+Animation.mobile = {}
+Animation.mobile.width = '1080px';
