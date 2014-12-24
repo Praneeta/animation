24bb0efd5498955f2f16b8589936fa3fa0bdfabf
akotian <ashleykotian@gmail.com>
2014-12-24 01:22:24 -0800

    Some questions

--- assets/js/religious.js
+++ assets/js/religious.js
@@ -9,8 +9,54 @@ Animation.video = { height: "128.5", width: "211.2", display: false };
 Animation.special_gift = ['bear', 'star-new', 'box'];
 Animation.question_set = [
   {
-    "question":"What is Dumbledore's sister's name?",
-    "answers":["Anna", "Ariana", "Adriana", "Angelina"],
+    "question":"Who told Joseph the baby's name was to be Jesus?",
+    "answers":["Mary", "His Father", "It was his decision", "An Angel"],
+    "correct": 3
+  },
+  {
+    "question":"How many wise men came to see Jesus?",
+    "answers":["3 or More", "One", "Two", "Only 3"],
+    "correct": 0
+  },
+  {
+    "question":"Which of these is NOT a name for Jesus in the bible",
+    "answers":["Emmanuel", "the Mighty God", "the Price of Peace", "Isaac"],
+    "correct": 3
+  },
+  {
+    "question":"What is Myrrh?",
+    "answers":["A drink", "A Food", "Spice used for burrying people", "Money"],
+    "correct": 2
+  },
+  {
+    "question":"Where did the wise men find Jesus",
+    "answers":["In a Manger", "In a stable", "Somewhere in UAE", "In a house"],
+    "correct": 3
+  },
+  {
+    "question":"What is a heavenly host ",
+    "answers":["An Angel Army", "An Angel Choir", "The angel at the gate of heaven", "None of the above"],
     "correct": 1
+  },
+  {
+    "question":"When Mary became pregnant, Mary and Joseph were..?",
+    "answers":["Married", "Engaged", "Not related at all", "Cousins"],
+    "correct": 1
+  },
+  {
+    "question":"Christmas has always been observed",
+    "answers":["July 4th", "In peak of winter", "December 25", "It changes every year"],
+    "correct": 3
+  },
+  {
+    "question":"What song did the Angel's sing?",
+    "answers":["Joy to the world", "O little town of Bethlehem", "They did not sing they said", "Amazin Grace"],
+    "correct": 2
+  },
+
+  {
+    "question":"What are Magi",
+    "answers":["Astrologers", "Magicians", "Fortune tells", "Kings from Greece"],
+    "correct": 0
   }
 ]
