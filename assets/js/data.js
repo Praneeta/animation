5bad98dd67c1a1ad474ca14cb1cad250d27148e2
praneeta <praneetamhatre@gmail.com>
2015-02-07 18:22:24 -0800

    updates

--- assets/js/data.js
+++ assets/js/data.js
@@ -8,7 +8,7 @@ app.data.meta = {
   ogurl: '',
   title: 'Happy Valentine\'s Day | makemyholidaycard.com',
   keywords: 'cards, free, quiz, 14, Feb, valentine, valentines day, love, hearts, cute, romance, romantic, funny, chemistry, ecards, last minute cards',
-  description: 'Shy or bold, we got a card for you. But you have to pass this test before you can express your love.',
+  description: 'Love is in the air. Happy Valentine\'s Day.',
   imageType: 'be-my-valentine',
   image: 'jpg',
   icon: 'heart.png'
