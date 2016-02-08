ff7a9dc35ccf2a606c06847f3aaabe26345f8202
praneeta <praneetamhatre@gmail.com>
2016-02-07 22:47:23 -0800

    new cards

--- assets/js/data.js
+++ assets/js/data.js
@@ -9,9 +9,9 @@ app.data.meta = {
   title: 'Happy Valentine\'s Day | makemyholidaycard.com',
   keywords: 'cards, free, quiz, 14, Feb, valentine, valentines day, love, hearts, cute, romance, romantic, funny, chemistry, ecards, last minute cards',
   description: 'Love is in the air. Happy Valentine\'s Day.',
-  imageType: 'bee-my-valentine',
-  image: 'jpg',
+  imageType: 'jpg',
+  image: 'yoda',
   icon: 'heart.png'
 }
 
-app.data.cards = ['bee-my-valentine', 'chemistry', 'olive-you', 'loaf-you','cute', 'chemistry-be-valentine' , 'love', 'u-blew-my-mind', 'chemistry-valentine', 'king', 'queen', 'listen' ]
+app.data.cards = ['palpatine', 'solo-valentine', 'yoda', 'heart-side', 'big_heart', 'bomb',	'heart-jars', 'cards', 	'heart-key', 'stick_together', 'heart_balloons', 'heart_jars_love', 'crush', 'you_complete_me', 'bee-my-valentine', 'chemistry', 'olive-you', 'loaf-you','cute', 'chemistry-be-valentine' , 'love', 'u-blew-my-mind', 'chemistry-valentine', 'king', 'queen', 'listen' ]
